cdbd65ee3732f4289db5bbccfd4c4d544a9068c1
Temi <ytemiloluwa@gmail.com>
2022-11-01 19:03:44 +0100

    [Chore: Quadratic ]

--- Complexity.playground/Contents.swift
+++ Complexity.playground/Contents.swift
@@ -1,22 +1,38 @@
 import Foundation
 
 // Constant time
-func createFirst(names: [String]) {
-    
-    if let first = names.first {
-        
-        print(first)
-    }else {
-        
-        print("no names")
-    }
-}
+//func createFirst(names: [String]) {
+//
+//    if let first = names.first {
+//
+//        print(first)
+//    }else {
+//
+//        print("no names")
+//    }
+//}
+//
+//createFirst(names: ["Sola", "Yemi", "Bola"])
 
-createFirst(names: ["Sola", "Yemi", "Bola"])
+// Linear time
+
+//func printNames(names: [String]) {
+//  for name in names {
+//    print(name)
+//  }
+//}
+//printNames(names: ["Sola", "Yemi", "Bola"])
+
+// Quadratic time
 
 func printNames(names: [String]) {
-  for name in names {
-    print(name)
-  }
+    
+    for _ in names {
+        
+        for name in names {
+            
+            print(name)
+        }
+    }
 }
 printNames(names: ["Sola", "Yemi", "Bola"])
