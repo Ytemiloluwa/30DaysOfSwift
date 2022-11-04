b3b2bb9d5265f81d3436b67500b041c6e1a2159b
Temi <ytemiloluwa@gmail.com>
2022-11-04 23:04:55 +0100

    [ Chore: Type Inference ]

--- Stacks.playground/Contents.swift
+++ Stacks.playground/Contents.swift
@@ -1,11 +1,20 @@
-var stack = Stack()
-stack.push(1)
-stack.push("Temi")
-stack.push(true)
-stack.push(1.0)
+//var stack = Stack()
+//stack.push(1)
+//stack.push("Temi")
+//stack.push(true)
+//stack.push(1.0)
+//print(stack)
+//
+//if let poppedElement = stack.pop() {
+//  print("Popped: \(poppedElement)")
+//}
+
+var arrray = ["A", "B", "C", "D"]
+
+var stack = Stack(elements: arrray)
+
 print(stack)
 
 if let poppedElement = stack.pop() {
   print("Popped: \(poppedElement)")
 }
-
