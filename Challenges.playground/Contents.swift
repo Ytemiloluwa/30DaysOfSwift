f1406585b8d2d1c8b9c32ea88a90a8a1abd0d400
Temi <ytemiloluwa@gmail.com>
2022-11-06 02:12:19 +0100

    [ Chore : Balance the parentheses ]

--- Challenges.playground/Contents.swift
+++ Challenges.playground/Contents.swift
@@ -2,25 +2,38 @@ import Foundation
 
 // STACK CHALLENGES
 
-public struct Stack: Any{
+public struct Stack<Elements> {
     
-    private var storage: [Any] = []
+    private var storage: [Elements] = []
     
     public init() {}
     
-    public mutating func push(_ elements: Any) {
+    public mutating func push(_ elements: Elements) {
         
         storage.append(elements)
         
     }
     
     @discardableResult
-    public mutating func pop() -> Any? {
+    public mutating func pop() -> Elements? {
         
         return storage.popLast()
     }
+    
+    public func peek() -> Elements? {
+        
+        storage.last
+        
+    }
+    
+    public var isEmpty: Bool {
+        
+        peek() == nil
+    
+    }
+    
+    
 }
-
 // REVERSE AN ARRAY
 
 func reversedArray(_ arrray: [Int]) -> [Int] {
@@ -33,7 +46,7 @@ print(reversedArray(Array(1...100)))
 
 func ReversedStackArray(_ list: [String]) {
     
-    var stack = Stack()
+    var stack = Stack<String>()
     
     for value in list {
         
@@ -41,6 +54,7 @@ func ReversedStackArray(_ list: [String]) {
         
     }
     while let value = stack.pop() {
+        
         print(value)
     }
 }
@@ -49,3 +63,30 @@ var arrray = ["A", "B", "C", "D"]
 
 print(arrray)
 ReversedStackArray(arrray)
+
+
+func checkParentheses(_ string: String)-> Bool {
+    
+    var stack = Stack<Character>()
+    
+    
+    for character in string {
+        
+        if character == "(" {
+            
+            stack.push(character)
+        }else if character == ")"{
+            if stack.isEmpty {
+                
+                return false
+            } else {
+                
+                stack.pop()
+            }
+            
+        }
+    }
+    return stack.isEmpty
+}
+
+print(checkParentheses("(Hello World !)"))
