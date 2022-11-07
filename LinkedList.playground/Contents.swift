282c9fd0f1013966a45fdad522945f45910690a1
Temi <ytemiloluwa@gmail.com>
2022-11-07 11:17:21 +0100

    [ Chore : Creating and Linking Nodes Update ]

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -1,3 +1,24 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+let node1 = Node(value: 1)
+let node2 = Node(value: 2)
+let node3 = Node(value: 3)
+let node4 = Node(value: 4)
+let node5 = Node(value: 5)
+
+
+node1.nextValue = node2
+node2.nextValue = node3
+node3.nextValue = node4
+node4.nextValue = node5
+
+print(node1)
+print(node2)
+print(node3)
+print(node4)
+print(node5)
+
+
+
+
+
