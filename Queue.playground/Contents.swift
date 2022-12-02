9244ea0c3a80d2a90fb7a29a7068f739ad3901ee
Temiloluwa <ytemiloluwa@gmail.com>
2022-12-02 18:36:10 +0100

    QueueStack (#42)
    * [ Chore : QueueStack ]
    
    * [ Chore : Dequeue in QueueStack ]

--- Queue.playground/Contents.swift
+++ Queue.playground/Contents.swift
@@ -44,3 +44,45 @@ queue
 queue.peek
 
 print(queue)
+
+
+// Double Stack Implementation
+
+public struct QueueStack<T>: Queue {
+
+    private var leftStack: [T] = []
+    
+    private var rightStack: [T] = []
+    
+    public init() {}
+    
+    public mutating func enqueue(_ element: T) -> Bool {
+        
+        rightStack.append(element)
+        return true
+    }
+    
+    public mutating func dequeue() -> T? {
+        if leftStack.isEmpty {
+            
+            leftStack = rightStack.reversed()
+            rightStack.removeAll()
+        }
+        
+        return leftStack.popLast()
+            
+    }
+    
+    public var peek: T? {
+        
+        !leftStack.isEmpty ? leftStack.last : rightStack.first
+        
+    }
+    
+    public var isEmpty: Bool {
+        
+        leftStack.isEmpty && rightStack.isEmpty
+    }
+    
+    
+}
