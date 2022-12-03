ef7792f0bf6a567c2efd5e68f793dee42a017807
Temi <ytemiloluwa@gmail.com>
2022-12-03 19:23:33 +0100

    [ Chore : forEachLevelOrder traversal ]

--- Trees.playground/Sources/TreeNodes.swift
+++ Trees.playground/Sources/TreeNodes.swift
@@ -28,3 +28,102 @@ extension TreeNode {
         }
     }
 }
+
+extension TreeNode {
+    
+    public func forEachLevelOrder(visit: (TreeNode) -> Void) {
+        
+        visit(self)
+        var queue = Queue<TreeNode>()
+        children.forEach{ queue.enqueue($0)}
+        
+        while let node = queue.dequeue() {
+            
+            visit(node)
+            
+            node.children.forEach{ queue.enqueue ($0)}
+        }
+    }
+}
+
+public struct QueueArray<T>: Queue {
+    
+    private var array: [T] = []
+    
+    public var isEmpty: Bool {
+        
+        array.isEmpty
+    }
+    
+    public mutating func enqueue(_ element: T) -> Bool {
+        
+        array.append(element)
+        
+        return true
+    }
+    
+    public mutating func dequeue() -> T? {
+        
+        array.isEmpty ? nil : array.removeFirst()
+    }
+    
+    public var peek: T? {
+        
+        array.first
+    }
+    
+}
+
+extension QueueArray : CustomStringConvertible {
+    
+    public var description: String {
+        
+        String(describing: array)
+    }
+}
+
+public struct QueueStack<T>: Queue {
+
+    private var leftStack: [T] = []
+    
+    private var rightStack: [T] = []
+    
+    public init() {}
+    
+    public mutating func enqueue(_ element: T) -> Bool {
+        
+        rightStack.append(element)
+        return true
+    }
+    
+    public mutating func dequeue() -> T? {
+        if leftStack.isEmpty {
+            
+            leftStack = rightStack.reversed()
+            rightStack.removeAll()
+        }
+        
+        return leftStack.popLast()
+            
+    }
+    
+    public var peek: T? {
+        
+        !leftStack.isEmpty ? leftStack.last : rightStack.first
+        
+    }
+    
+    public var isEmpty: Bool {
+        
+        leftStack.isEmpty && rightStack.isEmpty
+    }
+    
+}
+
+extension QueueStack: CustomStringConvertible {
+    
+    public var description: String {
+        
+        String(describing: leftStack.reversed() + rightStack)
+    }
+}
